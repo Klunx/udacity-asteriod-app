981111a8f9eea564d839e9e0edb10593a5191e81
Fernando Renteria <klunx316@hotmail.com>
2023-03-15 09:02:36 +1100

    Add Detail Asteroid flow

--- app/src/main/java/com/udacity/asteroidradar/main/view/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/view/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.udacity.asteroidradar.main.view
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.asteroid.domain.model.Asteroid
 import com.udacity.asteroidradar.asteroidlist.data.repository.AsteroidListRepositoryImpl
 import com.udacity.asteroidradar.common.database.getDatabase
 import com.udacity.asteroidradar.common.network.NasaApi
@@ -16,6 +19,13 @@ class MainViewModel(application: Application) : ViewModel() {
     private val pictureOfTheDayRepository = PictureOfTheDayRepositoryImpl(database, NasaApi.retrofitService)
     private val asteroidListRepository = AsteroidListRepositoryImpl(database)
 
+    val pictureOfTheDay = pictureOfTheDayRepository.pictureOfTheDay
+    val listOfAsteroids = asteroidListRepository.listOfAsteroids
+
+    private val _navigateToAsteroid = MutableLiveData<Asteroid>()
+    val navigateToAsteroid: LiveData<Asteroid>
+        get() = _navigateToAsteroid
+
     init {
         viewModelScope.launch {
             pictureOfTheDayRepository.getPictureOfTheDay()
@@ -25,7 +35,11 @@ class MainViewModel(application: Application) : ViewModel() {
         }
     }
 
-    val pictureOfTheDay = pictureOfTheDayRepository.pictureOfTheDay
-    val listOfAsteroids = asteroidListRepository.listOfAsteroids
+    fun navigateToAsteroidDetails(asteroid: Asteroid) {
+        _navigateToAsteroid.value = asteroid
+    }
 
+    fun navigateToAsteroidComplete() {
+        _navigateToAsteroid.value = null
+    }
 }
